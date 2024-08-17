43fcc57703133f00bbb9f2b0ef482a16edc95b62
malivaishnavi7 <vaishnavimali555@gmail.com>
2024-08-17 09:25:44 +0530

    second commit

---
+++ src/Component/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const footer=()=>{
+    return(
+        <div>this is footer</div>
+    )
+}
+
+export default footer;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Component/App';
+import footer from './Component/footer';
+
+ReactDOM.render(
+<footer/>,
+ document.querySelector('#root'));
