938a1d754be949d032b67cc63a6c213fdc8ebea1
yashwant-mali <yashwantmali555@gmail.com>
2024-08-17 19:27:38 +0530

    three

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Component/App';
-import footer from './Component/footer';
 
 ReactDOM.render(
-<footer/>,
+< App />,
  document.querySelector('#root'));
